88b96fe32712133eca6b2384416c65f2c2077870
Misha Biletskyi <biletskyimikhailo@gmail.com>
2022-12-13 15:30:08 +0200

    main page and theory

---
+++ script.js
@@ -0,0 +1,27 @@
+// document.addEventListener("DOMContentLoaded", () => {
+
+// const cards = document.querySelector("#parent");
+ 
+// const getCards = (cards) => {
+
+//     for(let i = 0; i <= 6; i++){
+//         let newCard = document.createElement("li",);
+//         newCard.className = "cards_item" 
+//         newCard.innerHTML += `
+//                 <div class="card">
+//                   <div class="card_image"><img src="img_1749.PNG"></div>
+//                   <div class="card_content">
+//                     <h2 class="card_title">Card Grid Layout</h2>
+//                     <p class="card_text">Demo of pixel perfect pure CSS simple responsive card grid layout</p>
+//                     <button class="btn card_btn">Read More</button>
+//                   </div>
+//                 </div>
+//           `
+//           cards.appendChild(newCard);
+//     }
+    
+    
+// }
+// getCards(cards);
+
+// });
